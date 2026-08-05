81221e8eb63d28ee3917f08e5a303b157a99b988
mlopez-888 <mlopezb@ucenfotec.ac.cr>
2026-08-05 17:11:06 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,12 @@
 public class HolaMundo {
     public static void main(String[] args) {
+        System.out.println("¡Hola, mundo desde Java!");
+        saludar("nombre");
+        sumar(5, 3);
+    }
+
+    public static void saludar(String MaryPaz) {
+        System.out.println("Hola, " + MaryPaz + ". ¡Bienvenida al ejercicio de Git y GitHub!");
     }
 
     public static void sumar(int a, int b) {
