1eb5a6e9dbb05f75ec419896de6694817a02b435
mlopez-888 <mlopezb@ucenfotec.ac.cr>
2026-08-05 17:08:09 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,2 +1,10 @@
 public class HolaMundo {
     public static void main(String[] args) {
+    }
+
+    public static void sumar(int a, int b) {
+        int resultado = a + b;
+        System.out.println("La suma de " + a + " + " + b + " es: " + resultado);
+    }
+}
+
