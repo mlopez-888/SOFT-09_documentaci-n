e82f451ad3b17479ecd9c253ee7831c83fa21087
mlopez-888 <mlopezb@ucenfotec.ac.cr>
2026-08-05 17:09:01 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,2 +1,16 @@
+function main() {
+    console.log("¡Hola, mundo desde JavaScript!");
+    saludar("MaryPaz");
+    sumar(5, 3);
+}
+
+function saludar(nombre) {
+    console.log("Hola, " + nombre + ". ¡Bienvenida al ejercicio de Git y GitHub!");
+}
+
+function sumar(a, b) {
+    const resultado = a + b;
+    console.log("La suma de " + a + " + " + b + " es: " + resultado);
+}
 
 main();
